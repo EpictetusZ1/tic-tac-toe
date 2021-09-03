57ec46094e98f4d4f66abbd064973ae06c1ff903
J Heaton <jackrobertheaton@gmail.com>
2021-09-03 17:36:16 -0400

    Add board state to Game function, style HTML

--- script.js
+++ script.js
@@ -3,8 +3,17 @@
 const Game = (() => {
 
     const boardSize = 9
-    const createBoard = () => _initBoardArr()
-    const _initBoardArr = () => new Array(boardSize).fill(null)
+    const boardState = () => createBoard()
+    const createBoard = () => {
+        let boardElement = document.getElementById("board")
+
+        for (let i = 1; i <= boardSize; i++) {
+            let sqDisplay = document.createElement("div")
+            boardElement.appendChild(sqDisplay).classList.add("square")
+        }
+        return new Array(boardSize).fill(null)
+
+    }
 
     const Player = (marker, name) => {
         return {
@@ -12,10 +21,13 @@ const Game = (() => {
             name
         }
     }
-    return {
-        publicProperty: createBoard,
-        playerOne: Player,
-        playerTwo: Player
-    }
 
+    return {
+        createBoard: createBoard,
+        boardState: boardState,
+        playerOne: Player,
+        playerTwo: Player,
+    }
 })()
+
+Game.boardState()
