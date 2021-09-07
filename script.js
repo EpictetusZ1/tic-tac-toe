6f0d2052547007ae352c384bfe27cdbb7fdb4036
J Heaton <jackrobertheaton@gmail.com>
2021-09-07 14:21:04 -0400

    Refactor, Add play again logic

--- script.js
+++ script.js
@@ -21,8 +21,16 @@ const Game = (() => {
         }
     }
 
-    let playerOne = Player("Jack", 0, true, 0)
-    let playerTwo = Player("John", 1, false,0)
+    let playerOne = Player("Player 1", 0, true, 0)
+    let playerTwo = Player("Player 2", 1, false,0)
+
+
+    const resetPlayer = () => {
+        playerOne.name = "Player 1"
+        playerTwo.name = "Player 2"
+        playerOne.wins = 0
+        playerTwo.wins = 0
+    }
 
     const p1Name = document.getElementById("playerOne")
     const p2Name = document.getElementById("playerTwo")
@@ -31,8 +39,10 @@ const Game = (() => {
         e.preventDefault()
         if (num === 0) {
             playerOne.name = p1Name[0].value
+            p1Name.reset()
         } else if (num === 1) {
             playerTwo.name = p2Name[0].value
+            p2Name.reset()
         }
     }
     // Collect names from form.
@@ -42,22 +52,43 @@ const Game = (() => {
         return 1
     })()
 
+    const boardContainer = document.querySelector(".board-container")
+    const title = document.querySelector(".title-container")
+
     const showResult = (player) => {
-        const title = document.querySelector(".title-container")
         let winnerDiv = document.createElement("div")
         if (!tie) {
             player.wins++
             winnerDiv.textContent = `${player.name} Won! ${player.name} has Won: ${player.wins} times!`
             title.appendChild(winnerDiv).classList.add("winner")
+            displayPlayAgain(boardContainer)
         } else {
             winnerDiv.textContent = "The game is a draw!"
             title.appendChild(winnerDiv).classList.add("winner")
+            displayPlayAgain(boardContainer)
         }
         setTimeout(() => title.removeChild(winnerDiv), 2000)
     }
 
+    const displayPlayAgain = (board) => {
+        const playAgainDiv = document.createElement("div")
+        playAgainDiv.setAttribute("id", "play-again-btn")
+        playAgainDiv.textContent = "Play Again ?"
+        board.appendChild(playAgainDiv).classList.add("play-again")
+        playAgainDiv.addEventListener("click", () => playAgain())
+        playAgainDiv.addEventListener("click", () => board.removeChild(playAgainDiv))
+    }
+
+    const playAgain = () => {
+        gameBoard.updateBoard()._resetPlay()
+        playerOne.isTurn = true
+        win = false
+        _counter = 0
+    }
+
     let _counter = 0
     let tie = false
+    let win = false
 
     const checkDraw = () => {
         _counter++
@@ -68,7 +99,7 @@ const Game = (() => {
     }
 
     const winCondition = (board) => {
-        if (!tie) {
+        if (!tie && !win) {
             const winConditions = [
                 [0, 1, 2],
                 [3, 4, 5],
@@ -86,8 +117,10 @@ const Game = (() => {
                 let c2 = cdn[2]
                 for (let x = 0; x < cdn.length; x++) {
                     if (board[c0] === 0 && board[c1] === 0 && board[c2] === 0) {
+                        win = true
                         return showResult(playerOne)
                     } else if (board[c0] === 1 && board[c1] === 1 && board[c2] === 1) {
+                        win = true
                         return showResult(playerTwo)
                     }
                 }
@@ -97,20 +130,24 @@ const Game = (() => {
     }
 
     const gameBoard = (() => {
-        const _boardSize = 9
-        const initBoardState = new Array(_boardSize).fill(undefined)
-        const boardElement = document.getElementById("board")
-
-        let _boardState = initBoardState
-
         // Creates Board in HTML
         const createBoard = () => {
+            const _boardSize = 9
+            const boardElement = document.getElementById("board")
+            const initBoardState = new Array(_boardSize).fill(undefined)
+
             for (let i = 1; i <= _boardSize; i++) {
                 let sqElement = document.createElement("div")
                 boardElement.appendChild(sqElement).classList.add("square")
                 sqElement.setAttribute("data", `${i}`)
             }
+            return {
+                initBoardState,
+                boardElement
+            }
         }
+
+        let _boardState = createBoard()
 
         const updateBoard = () => {
             // Add event listeners to all 'square' elements
@@ -140,7 +177,11 @@ const Game = (() => {
 
             const updateArray = (index, marker) => _boardState[index -1] = marker
 
+            const alertToClicked = () => {
+            }
+
             const updateHTML = (e) => {
+                // Keep track of squares that have been clicked
                 if (e.target.getAttribute("clicked") !== "1") {
                     let index = parseInt(e.target.getAttribute("data"))
                     _handleTurn(index, e)
@@ -148,15 +189,25 @@ const Game = (() => {
                     console.log("You already clicked this square")
                 }
             }
-
+            const boardElement = document.getElementById("board")
             const resetBtn = document.getElementById("restart-game")
             resetBtn.addEventListener("click", () => resetGame())
-
+            // Absolute Reset of the Game State
             const resetGame = () => {
                 boardElement.innerHTML = ""
                 _boardState = []
+                resetPlayer()
                 createBoard()
                 updateBoard()
+            }
+            const _resetPlay = () => {
+                boardElement.innerHTML = ""
+                _boardState = []
+                gameBoard.createBoard()
+                gameBoard.updateBoard()
+            }
+            return {
+                _resetPlay
             }
 
         }
@@ -169,6 +220,5 @@ const Game = (() => {
         gameState: gameBoard,
     }
 })()
-
-Game.gameState.createBoard()
+// Game.gameState.createBoard()
 Game.gameState.updateBoard()
